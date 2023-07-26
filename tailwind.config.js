eeb767fc8978a2b004bf2f4df50184b3bb6454b1
mhassansubhani <hassansubhani8@hotmail.com>
2023-07-27 01:46:34 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+{/* <script src="https://cdn.tailwindcss.com">   </script> */}
